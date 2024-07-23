a2834d90cfe7d428ba3d0a78c3ed221e18df8d49
Dylan Spyer <dylanspyer@gmail.com>
2024-07-22 22:56:02 -0500

    feat: started Typescript file for cerebellum client
    Co-authored-by: Avery Littlemore <avery.d.littlemore@gmail.com>
    Co-authored-by: Austin Nelson < nelsonaustin11@gmail.com>
    Co-authored-by: Dylan Spyer <dylanspyer@gmail.com>
    Co-authored-by: Will Conrad <w.conrad@outlook.com>

--- Cerebellum/cerebellumTypeScript.ts
+++ Cerebellum/cerebellumTypeScript.ts
@@ -1,110 +1,231 @@
-// import { io } from "socket.io-client";
-// import axios from "axios";
+import { io, Socket } from "socket.io-client";
+import axios from "axios";
 
-// // export interface Payload {
-// //   username?: string;
-// // }
+interface Message {
+  content: string;
+  createdAt: string;
+}
+
+interface LastEvaluatedKey extends Message {
+  channelName: string;
+  messageId: string;
+}
+
+interface Acknowledgement {
+  success: boolean;
+  pastMessages?: Message[];
+  lastEvaluatedKey?: LastEvaluatedKey;
+}
+
+const fetchSignedToken = async (authRoute, authMethod = "POST", payload) => {
+  console.log(authRoute);
+  try {
+    const { data } =
+      authMethod === "GET"
+        ? await axios.get(authRoute)
+        : await axios.post(authRoute, payload);
+    return data;
+  } catch (error) {
+    console.error("Error fetching token: ", error);
+    throw error;
+  }
+};
+
+const initializeConnection = async (options) => {
+  try {
+    const { endpoint, method, payload } = options.authRoute;
+    const token = await fetchSignedToken(endpoint, method, payload);
+
+    options.auth = {};
+    options.auth.token = token;
+    options.autoConnect = true;
+  } catch (error) {
+    console.error("Error initializing connection: ", error);
+  }
+};
+
+export const Cerebellum = async (endpoint, options) => {
+  options.auth = {};
+
+  if (options.autoConnect === true) await initializeConnection(options);
+
+  return new CerebellumInit(endpoint, options);
+};
+
+class CerebellumInit {
+  socket: Socket;
+
+  constructor(endpoint, options = {}) {
+    this.socket = io(endpoint, options);
+    this.init();
+  }
+
+  init() {
+    this.addAuthErrorListener;
+  }
+
+  on(event: string, callback: () => any) {
+    this.socket.on(event, callback);
+  }
+
+  authErrorCallback(callback: () => any) {
+    this.socket.on("connect_error", (reason) => {
+      console.log("Connection error:", reason.message);
+      if (reason.message === "Authentication error") {
+        callback();
+      }
+    });
+  }
+
+  addAuthErrorListener() {
+    this.socket.on("connect_error", (reason) => {
+      console.log("Connection error:", reason.message);
+      if (reason.message === "Authentication error") {
+        this.socket.disconnect();
+      }
+    });
+  }
+
+  connect() {
+    this.socket.connect();
+  }
+
+  disconnect() {
+    this.socket.disconnect();
+  }
+
+  off(event: string, callback: () => any) {
+    this.socket.off(event, callback);
+  }
+
+  async auth(authEndpoint: string, method: "GET" | "POST", payload) {
+    try {
+      const token = await fetchSignedToken(authEndpoint, method, payload);
+
+      this.socket.auth["token"] = token; // does this work?
+    } catch (error) {
+      console.error("Error initializing connection: ", error);
+    }
+  }
+
+  subscribeChannel(
+    channelName: string,
+    callback: (pastMessages: Message[]) => any
+  ) {
+    this.socket.emit(
+      "channel:subscribe",
+      channelName,
+      (ack: Acknowledgement) => {
+        if (ack.success && ack.pastMessages) {
+          callback(ack.pastMessages);
+          this.socket.on(`message:receive:${channelName}`, callback);
+        } else {
+          console.error(`Failed to subscribe to channel ${channelName}`);
+        }
+      }
+    );
+  }
+
+  unsubscribeChannel(channelName, callback) {
+    this.socket.emit(`channel:unsubscribe`, channelName, (ack) => {
+      if (ack.success) {
+        callback();
+        console.log(`Unsubscribed from channel ${channelName}`);
+      } else {
+        console.error(`Failed to unsubscribe from channel ${channelName}`);
+      }
+    });
+    this.socket.off(`message:receive:${channelName}`, callback);
+  }
+
+  publish(channelName, message) {
+    this.socket.emit("message:queue", channelName, message);
+  }
+
+  //enters the user into the presence set, lets everyone know that someone has entered
+  enterPresenceSet(channelName, state) {
+    this.socket.emit("presenceSet:enter", channelName, state);
+  }
+
+  leavePresenceSet(channelName) {
+    this.socket.emit("presenceSet:leave", channelName);
+  }
+
+  //Get the current members in the presence set
+  getPresenceSetMembers(channelName) {
+    return new Promise((resolve, reject) => {
+      this.socket.emit("presence:members:get", channelName, (ack) => {
+        if (ack.success === true) {
+          resolve(ack.users);
+        } else {
+          reject(
+            new Error(`Failed to subscribe to presence set ${channelName}`)
+          );
+        }
+      });
+    });
+  }
+
+  subscribeToPresenceJoins(channelName, callback) {
+    this.socket.on(`presence:${channelName}:join`, callback);
+  }
+  //subscribePresenceUpdate
+  subscribeToPresenceUpdates(channelName, callback) {
+    this.socket.on(`presence:${channelName}:update`, callback);
+  }
+
+  //subscribePresenceLeave
+  subscribeToPresenceLeaves(channelName, callback) {
+    this.socket.on(`presence:${channelName}:leave`, callback);
+  }
+
+  // Method to unsubscribe from presence leave events
+  unsubscribeFromPresenceLeaves(channelName, callback) {
+    this.socket.off(`presence:${channelName}:leave`, callback);
+  }
+
+  // Method to unsubscribe from presence join events
+  unsubscribeFromPresenceJoins(channelName, callback) {
+    this.socket.off(`presence:${channelName}:join`, callback);
+  }
+
+  // Method to unsubscribe from presence update events
+  unsubscribeFromPresenceUpdates(channelName, callback) {
+    this.socket.off(`presence:${channelName}:update`, callback);
+  }
+
+  updatePresenceInfo(channelName, state) {
+    this.socket.emit("presence:update", channelName, state);
+  }
+
+  getSocket() {
+    return this.socket;
+  }
+}
+
+// Messages are broadcast on channels. The following example code subscribes
+// the client to a channel called quickstart and sets a filter so that the client only receives
+// messages with the name greeting. When a message is received, its contents will be printed
+// after the text Received a greeting message in realtime:.
+
+// Note: The channel is created in the Ably service when the client subscribes to it.
+
+// get the channel to subscribe to
+// const channel = ably.channels.get("quickstart");
+// // const channe
 
 // /*
-// {
-//   autoConnect: false,
-//   reconnection: true, // Enable reconnection attempts
-//   reconnectionAttempts: 5, // Number of attempts before giving up
-//   reconnectionDelay: 5000, // Delay between reconnection
-//   reconnectionDelayMax: 5000, // Maximum delay between reconnection
-//   timeout: 20000, // Before a connection attempt is considered failed
-// }
+//   Subscribe to a channel.
+//   The promise resolves when the channel is attached
+//   (and resolves synchronously if the channel is already attached).
 // */
-// /*javascript
-// const URL = "http://localhost:8000";
-
-// // Generic token signed with API key (no user credentials needed)
-// const cerebellum = new Cerebellum(URL, {
-//   autoConnect: true,
-//   authRoute: "localhost:3000",
-//   reconnection: true, // Enable reconnection attempts
-//   reconnectionAttempts: 5, // Number of attempts before giving up
-//   reconnectionDelay: 5000, // Delay between reconnection
-//   reconnectionDelayMax: 5000, // Maximum delay between reconnection
-//   timeout: 20000, // Before a connection attempt is considered failed
+// await channel.subscribe("greeting", (message) => {
+//   console.log("Received a greeting message in realtime: " + message.data);
 // });
-// */
 
-// export default class Cerebellum extends io {
-//   // endpoint: string;
-
-//   constructor(endpoint: string, options) {
-//     /*
-//       - if autoconnect is true, ping the auth route first, before calling super, because we need the auth token
-//       - we need to add auth token header to auth
-//     */
-//     super(URL, { options });
-//     this.endpoint = endpoint; // The load balancer endpoint or localhost - can this be automated somehow?
-//   }
-
-//   // Developer puts their auth route here
-//   // Make a network request to the auth route
-//   // Returns a token
-//   // Developer should set the authEndpoint up such that it returns a token signed by their API key
-//   // Problem: dev has to sign the token with the API key and set the expiration to 1 minute themselves
-//   // Maybe provide simple class to create and return signed token with their api key.
-//   // async fetchSignedToken(
-//   //   authRoute: string,
-//   //   authMethod: "GET" | "POST" = "GET",
-//   //   payload?: Payload
-//   // ): Promise<string | void> {
-//   //   const authEndpoint = this.endpoint + authRoute;
-
-//   //   if (authMethod === "GET") {
-//   //     try {
-//   //       const { data } = await axios.get(authEndpoint);
-//   //       return data;
-//   //     } catch (error) {
-//   //       console.error("Error fetching token: ", error);
-//   //       throw error;
-//   //     }
-//   //   } else if (authMethod === "POST") {
-//   //     try {
-//   //       const { data } = await axios.post(authEndpoint, payload);
-//   //       return data;
-//   //     } catch (error) {
-//   //       console.error("Error fetching token: ", error);
-//   //       throw error;
-//   //     }
-//   //   } else {
-//   //     throw new Error("Use GET or POST");
-//   //   }
-//   // }
-
-//   async setSocketAuth() {} // Maybe this calls `fetchSignedToken` and sets the header, that way they only call this
-
-//   // Includes all the socket.io headers for reconnection and auth
-//   // Give a way to adjust ping and pong intervals? Or hard code these?
-//   // connect(signedToken, endpoint) {
-//   //   io(endpoint, {
-//   //     autoConnect: false,
-//   //     auth: {}, // Call `fetchSignedToken` before calling `connect`. It fills this in.
-//   //   });
-//   // }
-// }
-
-// // I use the CLI to spin up AWS infrastructure and get my API key
-// // I run the Cerebellum server image for my backend
-// // I use the Cerebellum client library to help develop on the frontend
-
-// /*
-// 1. Developer creates login route
-// 2. Developer uses login route to check creds
-// 3. Developer stores API key on login route server
-// 4. Developer has login server sign a JWT token with the username as the payload and the API key as the secret
-
-
-
-// methods:
-// - connect
-// - fetchSignedToken
-//   - should these be grouped together?
-//   - if we don't ever want to connect without fetching the token, yes
-//   - if we give users the ability to not use auth, no -> still need to think about what this would look like - need to change backend code
-// - 
-// */
+/* 
+  this.presence = {
+    room1: [dylan, will],
+    room2: [avery, austin]
+  }
+*/
